b8fbc626a442760f928473669df2c3f6b73a2eff
kunalkt5656 <kunalkt97@gmail.com>
2026-08-27 23:57:21 +0530

    feat: add main application component with AuditDashboard

---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { AuditDashboard } from './pages/AuditDashboard';
+
+function App() {
+  return (
+    <div className="app-container">
+      <AuditDashboard />
+    </div>
+  );
+}
+
+export default App;
